aa5f13053c2c5bfc19aa9d5bcf7276933a569940
Russell Billingsley <rbillingsley@users.noreply.github.com>
2018-05-04 21:13:28 +0100

    Add basic controls to player

--- src/gameobjects/player.ts
+++ src/gameobjects/player.ts
@@ -1,11 +1,24 @@
 import Drawable from '../primitives/drawable';
+import Keydown from '../keys';
 
 export default class Player extends Drawable {
   constructor(inWidth: number, inHeight: number, inPosition: Point) {
     super(inWidth, inHeight, inPosition);
   }
 
-  public update(delta: number) {}
+  private targetY: number = 10;
+  private buoyancy: number = 0.2;
+  private balast: number = 0.5;
+
+  public update(delta: number) {
+    if (Keydown.space) {
+      this.position.y += this.balast * delta;
+    }
+
+    if (this.position.y > this.targetY) {
+      this.position.y -= this.buoyancy * delta;
+    }
+  }
 
   public draw(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = '#595d63';
@@ -16,5 +29,6 @@ export default class Player extends Drawable {
     ctx.rect(this.position.x, this.position.y, this.width, this.height);
     ctx.stroke();
     ctx.fill();
+    ctx.closePath();
   }
 }
